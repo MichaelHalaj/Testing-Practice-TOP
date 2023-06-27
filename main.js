3a6a90207259be64bea040ac9e6161ef5f14a022
Michael Halaj <michaelhalaj7@gmail.com>
2023-06-27 11:53:19 -0400

    Complete reverseString

--- main.js
+++ main.js
@@ -2,4 +2,16 @@ function capitalize(word) {
     return word.substring(0, 1).toUpperCase() + word.substring(1);
 }
 
-export { capitalize };
+function reverseString(word) {
+    let array = new Array(word.length);
+    let l = 0, r = word.length - 1;
+    while(l <= r){
+        array[l] = word.charAt(r);
+        array[r] = word.charAt(l);
+        l++;
+        r--;
+    }
+    return array.join("");
+}
+
+export { capitalize, reverseString };
--- main.test.js
+++ main.test.js
@@ -1,5 +1,11 @@
-import { capitalize } from './main.js';
+import { capitalize, reverseString } from './main.js';
 
 it('Capitalizes the first letter in in a string', () => {
     expect(capitalize('hello world')).toBe('Hello world');
 });
+
+it('Reverses a string', () => {
+    expect(reverseString('hello world')).toBe('dlrow olleh');
+    expect(reverseString('world')).toBe('dlrow');
+    expect(reverseString('hello')).toBe('olleh');
+})
