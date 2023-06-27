501b418d8640c0c9bca40fdc39b75fb97271008b
Michael Halaj <michaelhalaj7@gmail.com>
2023-06-27 11:59:05 -0400

    Complete calculator

--- main.js
+++ main.js
@@ -14,4 +14,22 @@ function reverseString(word) {
     return array.join("");
 }
 
-export { capitalize, reverseString };
+class Calculator {
+     static add(a, b) {
+        return a + b;
+    }
+
+    static subtract(a, b) {
+        return a - b;
+    }
+
+    static multiply(a, b) {
+        return a * b;
+    }
+
+    static divide(a, b) {
+        return a / b;
+    }
+}
+
+export { capitalize, reverseString, Calculator };
--- main.test.js
+++ main.test.js
@@ -1,4 +1,4 @@
-import { capitalize, reverseString } from './main.js';
+import { capitalize, reverseString, Calculator } from './main.js';
 
 it('Capitalizes the first letter in in a string', () => {
     expect(capitalize('hello world')).toBe('Hello world');
@@ -9,3 +9,10 @@ it('Reverses a string', () => {
     expect(reverseString('world')).toBe('dlrow');
     expect(reverseString('hello')).toBe('olleh');
 })
+
+it('Performs add, subtract, divide, and multiply' ,() => {
+    expect(Calculator.add(4, 3)).toBe(7);
+    expect(Calculator.divide(12, 4)).toBe(3);
+    expect(Calculator.subtract(4, 3)).toBe(1);
+    expect(Calculator.multiply(5, 3)).toBe(15);
+});
